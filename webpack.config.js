7231eca5664d58d14d093ab537fadb4e57a26bbf
miraks <a@vldkn.net>
2018-06-22 20:23:10 +0300

    Update dependencies and switch to yarn

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,11 @@
 
 const path = require('path')
 const webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const ZipPlugin = require('./plugins/zip')
 
-const extractSass = new ExtractTextPlugin({ filename: '[name].css' })
-
 module.exports = {
+  mode: process.env.NODE_ENV || 'development',
   entry: {
     background: './src/background/index.js',
     content: './src/content/index.js'
@@ -16,25 +14,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          plugins: [['transform-object-rest-spread', { useBuiltIns: true }], ['transform-class-properties']]
-        }
-      },
-      {
-        test: /\.sass$/,
-        use: extractSass.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      }
-    ]
   },
   resolve: {
     alias: {
@@ -46,8 +25,10 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     }),
-    extractSass,
-    new CopyPlugin([{ from: 'manifest.json' }, { from: 'icons', to: 'icons' }]),
+    new CopyPlugin([
+      { from: 'manifest.json' },
+      { from: 'icons', to: 'icons' }
+    ]),
     new ZipPlugin({ path: 'mediacontrol@vldkn.net.xpi' })
   ]
 }
